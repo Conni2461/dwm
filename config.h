6c4feafbe4ec72c7f282c5bf08593c02d2d99e57
Simon Hauser <Simon.Hauser.XD@live.de>
2019-05-23 15:07:25 +0200

    Removed useless gap patch

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const unsigned int gappx     = 5;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int horizpadbar        = 6;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 7;        /* vertical padding for statusbar */
+static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 0;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=13";
 static const char col_gray1[]       = "#222222";
@@ -47,7 +47,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.6; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
@@ -98,13 +98,17 @@ static Key keys[] = {
         { MODKEY,               XK_b,                   togglebar,      {0} },
         { MODKEY|ShiftMask,     XK_j,                   rotatestack,    {.i = +1 } },
         { MODKEY|ShiftMask,     XK_k,                   rotatestack,    {.i = -1 } },
+        { MODKEY|ShiftMask,     XK_Down,                rotatestack,    {.i = +1 } },
+        { MODKEY|ShiftMask,     XK_Up,                  rotatestack,    {.i = -1 } },
         { MODKEY,               XK_j,                   focusstack,     {.i = +1 } },
         { MODKEY,               XK_k,                   focusstack,     {.i = -1 } },
+        { MODKEY,               XK_Down,                focusstack,     {.i = +1 } },
+        { MODKEY,               XK_Up,                  focusstack,     {.i = -1 } },
         { MODKEY,               XK_i,                   incnmaster,     {.i = +1 } },
         { MODKEY,               XK_o,                   incnmaster,     {.i = -1 } },
         { MODKEY,               XK_h,                   setmfact,       {.f = -0.05} },
         { MODKEY,               XK_l,                   setmfact,       {.f = +0.05} },
-        { MODKEY,               XK_Return,              zoom,           {0} },
+        { MODKEY|ShiftMask,     XK_Return,              zoom,           {0} },
         { MODKEY,               XK_Tab,                 view,           {0} },
         { MODKEY|ShiftMask,     XK_c,                   killclient,     {0} },
         { MODKEY,               XK_t,                   setlayout,      {.v = &layouts[0]} },
