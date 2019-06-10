df657c72e7fa9f854886ab1c4284a620e48cc7ef
Simon Hauser <Simon.Hauser.XD@live.de>
2019-06-10 18:19:41 +0200

    New formatting

--- config.h
+++ config.h
@@ -1,35 +1,35 @@
 /* See LICENSE file for copyright and license details. */
 
-#define XF86AudioMute			0x1008ff12
+#define XF86AudioMute   		0x1008ff12
 #define XF86AudioLowerVolume	0x1008ff11
 #define XF86AudioRaiseVolume	0x1008ff13
 
 /* appearance */
-static const unsigned int borderpx		= 2;	/* border pixel of windows */
-static const unsigned int gappx			= 5;	/* gaps between windows */
-static const unsigned int snap			= 32;	/* snap pixel */
+static const unsigned int borderpx  	= 2;	/* border pixel of windows */
+static const unsigned int gappx 		= 5;	/* gaps between windows */
+static const unsigned int snap  		= 32;	/* snap pixel */
 static const int showbar				= 1;	/* 0 means no bar */
-static const int topbar					= 1;	/* 0 means bottom bar */
+static const int topbar 				= 1;	/* 0 means bottom bar */
 static const int horizpadbar			= 6;	/* horizontal padding for statusbar */
-static const int vertpadbar				= 0;	/* vertical padding for statusbar */
-static const char *fonts[]				= { "monospace:size=17" };
-static const char dmenufont[]			= "monospace:size=17";
-static const char col_gray1[]			= "#222222";
-static const char col_gray2[]			= "#444444";
-static const char col_gray3[]			= "#bbbbbb";
-static const char col_gray4[]			= "#eeeeee";
+static const int vertpadbar 			= 0;	/* vertical padding for statusbar */
+static const char *fonts[]  			= { "monospace:size=17" };
+static const char dmenufont[]   		= "monospace:size=17";
+static const char col_gray1[]   		= "#222222";
+static const char col_gray2[]   		= "#444444";
+static const char col_gray3[]   		= "#bbbbbb";
+static const char col_gray4[]   		= "#eeeeee";
 static const char col_cyan[]			= "#005577";
-static const unsigned int baralpha		= 0xd0;
-static const unsigned int borderalpha	= OPAQUE;
-static const char *colors[][3]			= {
-	/*					fg			bg		   border	*/
-	[SchemeNorm] = {	col_gray3,	col_gray1, col_gray2 },
-	[SchemeSel]  = {	col_gray4,	col_cyan,  col_cyan  },
+static const unsigned int baralpha  	= 0xd0;
+static const unsigned int borderalpha   = OPAQUE;
+static const char *colors[][3]  		= {
+	/*  				fg  		bg  		border  	*/
+	[SchemeNorm] = {	col_gray3,  col_gray1,  col_gray2   },
+	[SchemeSel]  = {	col_gray4,  col_cyan,   col_cyan	},
 };
 static const unsigned int alphas[][3]	= {
-	/*					fg		bg			border	  */
-	[SchemeNorm] = {	OPAQUE, baralpha,	borderalpha },
-	[SchemeSel]  = {	OPAQUE, baralpha,	borderalpha },
+	/*  				fg  		bg  		border  	*/
+	[SchemeNorm] = {	OPAQUE, 	baralpha,   borderalpha },
+	[SchemeSel]  = {	OPAQUE, 	baralpha,   borderalpha },
 };
 
 /* tagging */
