898a07237aa7a22303b21e928e1a9f30d1e16e54
Simon Hauser <Simon-Hauser@outlook.de>
2020-02-04 21:44:22 +0100

    Also include gaps with only one window open

--- dwm.c
+++ dwm.c
@@ -1941,11 +1941,6 @@ tile(Monitor *m)
 	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
 	if (n == 0)
 		return;
-	if (n == 1) {
-		c = nexttiled(m->clients);
-		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
-		return;
-	}
 
 	if (n > m->nmaster) {
 		mw = m->nmaster ? m->ww * m->mfact : 0;
