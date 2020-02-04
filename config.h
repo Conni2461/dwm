29150d4bc7517527713cfc6b1bdbd5b787e58a4c
Simon Hauser <Simon-Hauser@outlook.de>
2020-02-04 22:34:05 +0100

    Updated font, so dmenu complete covers top bar

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static const int vertpadbar                  = 0;    /* vertical padding for sta
 static const int vertpad                     = 5;   /* vertical padding of bar */
 static const int sidepad                     = 5;   /* horizontal padding of bar */
 static const char *fonts[]                   = { "monospace:size=10" };
-static const char dmenufont[]                = "monospace:size=10";
+static const char dmenufont[]                = "monospace:size=11";
 static const char normbgcolor[]              = "#1c1b19";
 static const char normfgcolor[]              = "#fce8c3";
 static const char normbordercolor[]          = "#1c1b19";
